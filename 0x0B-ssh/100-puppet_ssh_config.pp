f747778f6b3c67e82bed6e899a428154e4e5650c
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-09-23 22:51:50 +0000

    t100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,7 @@
 #file to create config file
 node default {
-file {'/home/vagrant/.ssh/config':
-ensure  => 'present',
-content => "Host *\n\
-				PasswordAuthentication	no\n\
-				IdentityFile	~/.ssh/school",
-mode    => '0600'
+exec {'echo':
+command => 'echo PasswordAuthentication no"\n"IdentityFile ~/.ssh/school >> /etc/ssh/ssh_config',
+path    => '/usr/bin'
 }
 }
