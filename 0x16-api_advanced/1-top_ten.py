5fc7c495e657a1edb2b5cdea4b4c049ce6b6bf58
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-11-07 12:43:59 +0000

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" title of 10 hot posts """
+import requests
+
+
+def top_ten(subreddit):
+    """ returns title of 10 hot posts """
+    url = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
+    headers = {"user-agent": "me"}
+    data = requests.get(url, headers=headers)
+    if data.status_code == 404:
+        print(None)
+        return
+    data = data.json()
+    for i in range(len(data['data']['children'])):
+        print(data['data']['children'][i]['data']['title'])
+    return
