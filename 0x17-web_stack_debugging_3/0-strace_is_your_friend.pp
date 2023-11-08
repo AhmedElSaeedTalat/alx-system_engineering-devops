91c70e4d46b46ea2c8f45661f76182d13cd89cb1
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-11-08 16:13:44 +0000

    task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+node default {
+  exec {'exec':
+    command  => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php;
+    service apache2 restart',
+    provider => shell
+  }
+}
